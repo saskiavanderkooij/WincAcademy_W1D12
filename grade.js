16c32a3691e804b8f3a1af32c653e254c0da34a4
Saskia vd Kooij <84019788+saskiavanderkooij@users.noreply.github.com>
2021-06-17 20:03:01 +0200

    Update grade.js

--- grade.js
+++ grade.js
@@ -1,6 +1,6 @@
 // The Grade Assigner
-for (i = 60; i > 100; i++) {
-    console.log("Voor " + grade + " punten krijg je een " + assignGrade(i));
+for (let i = 60; i <= 100; i++) {
+    console.log("Voor " + i + " punten krijg je een " + assignGrade(i));
 }
 
 function assignGrade(score) {
@@ -8,7 +8,7 @@ function assignGrade(score) {
         return 'A';
     } else if (score > 80) {
         return 'B';
-    } else if (score > 70) {s
+    } else if (score > 70) {
         return 'C';
     } else if (score > 65) {
         return 'D';
